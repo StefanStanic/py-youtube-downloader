7b660cfc8b38aa95377a142bfde446ea676120cf
Stefan Stanic <stefke@Stefans-MacBook-Pro.local>
2019-01-22 23:28:34 +0100

    py youtube downloader built to read urls from csv file and downloads mp3 version of them

---
+++ py-youtube-to-mp3.py
@@ -0,0 +1,55 @@
+import youtube_dl
+import csv
+import os
+
+""" Uses either a list from a csv or from input to download youtube video to MP3 in HQ"""
+
+
+#Debuggers and a custom hook
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+    
+    def error(self,msg):
+        print(msg)
+
+def done_hook(done):
+    if done['status'] == 'finished':
+        print('Done downloading, now need to convert... please hold')
+
+#populate the list with urls from the actuall file
+def populate_url_list(url_file):
+    with open(url_file, 'r', encoding = 'utf-8', errors = 'ignore') as input:
+        reader = csv.reader(input)
+        return list(row[0] for row in reader)
+
+#initiates a stream and downloads the yotube + converts to mp3
+def yt_downloader(urls_to_download):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',    
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [done_hook],
+        'outtmpl': os.getcwd() + '/mp3-files/%(title)s.%(ext)s'
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(urls_to_download)
+
+
+def main():
+    urls_to_download = populate_url_list('youtube_url_list.csv')
+    yt_downloader(urls_to_download)
+
+
+
+if __name__ == "__main__":
+    print("Youtube Downloader started")
+    main()
